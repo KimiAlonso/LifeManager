e7634949711edaaaa39ae155c24bf96bc7b15a68
KimiAlonso <553571698@qq.com>
2016-12-26 23:28:38 +0800

    update

--- app/src/main/java/com/zbd/lifemanager/Fragment/Fm03/Fm03.java
+++ app/src/main/java/com/zbd/lifemanager/Fragment/Fm03/Fm03.java
@@ -9,10 +9,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,8 +26,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.zbd.lifemanager.Json.NBAJson.NBAjson;
+import com.zbd.lifemanager.Json.NBAJson.TeamJson;
 import com.zbd.lifemanager.MainActivity;
 import com.zbd.lifemanager.R;
 
@@ -38,14 +46,33 @@ public class Fm03 extends Fragment implements SwipeRefreshLayout.OnRefreshListen
     String url = "http://op.juhe.cn/onebox/basketball/nba?key=645b5a36905d5e6f2da6d7a10cbb564c";
     String TAG = "Fm03";
     List<Map<String,Object>> mDataList = new ArrayList<>();
+    List<Map<String,Object>> mTeamList = new ArrayList<>();
     ListView lv;
     MHandler mHandler;
+    TextView mainTeam;
+    ImageView team01logo;
+    ImageView team02logo;
+    ImageView team03logo;
+    ImageView team04logo;
+    ImageView team05logo;
+    ImageView team06logo;
+    View mView;
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fm03,null);
         context = ((MainActivity) getActivity()).getContext();
+
+        mView = view;
+
+
+
+
+        mainTeam = (TextView) view.findViewById(R.id.main_team);
+        mainTeam.setText("马刺");
 
         mHandler = new MHandler();
 
@@ -54,6 +81,29 @@ public class Fm03 extends Fragment implements SwipeRefreshLayout.OnRefreshListen
         srl.setOnRefreshListener(this);
         lv = (ListView) view.findViewById(R.id.NBAList);
         onRefresh();
+
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                boolean enable = false;
+                if(lv != null && lv.getChildCount() > 0){
+                    // check if the first item of the list is visible
+                    boolean firstItemVisible = lv.getFirstVisiblePosition() == 0;
+                    // check if the top of the first item is visible
+                    boolean topOfFirstItemVisible = lv.getChildAt(0).getTop() == 0;
+                    // enabling or disabling the refresh layout
+                    enable = firstItemVisible && topOfFirstItemVisible;
+                }
+                srl.setEnabled(enable);
+            }
+        });
+
+
 
 
 
@@ -72,14 +122,14 @@ public class Fm03 extends Fragment implements SwipeRefreshLayout.OnRefreshListen
                     @Override
                     public void onResponse(String s) {
                         Gson NBAGson = new Gson();
-                        Log.e(TAG, "onResponse: OK" );
+
                         NBAjson nbaJson = NBAGson.fromJson(s,NBAjson.class);
-                        for (int i=0;i<nbaJson.getResult().getList().get(0).getTr().size();i++){
-                            String player1 = nbaJson.getResult().getList().get(0).getTr().get(i).getPlayer1();
-                            String player2 = nbaJson.getResult().getList().get(0).getTr().get(i).getPlayer2();
-                            String player1Logo = nbaJson.getResult().getList().get(0).getTr().get(i).getPlayer1logo();
-                            String player2Logo = nbaJson.getResult().getList().get(0).getTr().get(i).getPlayer2logo();
-                            String score = nbaJson.getResult().getList().get(0).getTr().get(i).getScore();
+                        for (int i=0;i<nbaJson.getResult().getList().get(1).getTr().size();i++){
+                            String player1 = nbaJson.getResult().getList().get(1).getTr().get(i).getPlayer1();
+                            String player2 = nbaJson.getResult().getList().get(1).getTr().get(i).getPlayer2();
+                            String player1Logo = nbaJson.getResult().getList().get(1).getTr().get(i).getPlayer1logo();
+                            String player2Logo = nbaJson.getResult().getList().get(1).getTr().get(i).getPlayer2logo();
+                            String score = nbaJson.getResult().getList().get(1).getTr().get(i).getScore();
                             Map<String,Object> map = new HashMap<String, Object>();
                             map.put("player1",player1);
                             map.put("player2",player2);
@@ -87,11 +137,11 @@ public class Fm03 extends Fragment implements SwipeRefreshLayout.OnRefreshListen
                             map.put("player2logo",player2Logo);
                             map.put("score",score);
                             mDataList.add(map);
-                            mHandler.sendEmptyMessage(0);
+
                         }
 
 
-                        srl.setRefreshing(false);
+
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -99,7 +149,43 @@ public class Fm03 extends Fragment implements SwipeRefreshLayout.OnRefreshListen
 
                     }
                 });
+
+                String url1 = "http://op.juhe.cn/onebox/basketball/team?dtype=&team="+"马刺"+"&key=645b5a36905d5e6f2da6d7a10cbb564c";
+
+                StringRequest request1 = new StringRequest(Request.Method.GET, url1, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Gson TeamGson = new Gson();
+                        TeamJson teamJson = TeamGson.fromJson(s, TeamJson.class);
+                        for (int i = 0;i<=3;i++){
+                            String player1logo = teamJson.getResult().getList().get(i).getPlayer1logo();
+                            String player2logo = teamJson.getResult().getList().get(i).getPlayer2logo();
+                            String time = teamJson.getResult().getList().get(i).getM_time();
+
+                            Map<String,Object> map = new HashMap<String ,Object>();
+                            map.put("player1logo",player1logo);
+                            map.put("player2logo",player2logo);
+                            map.put("time",time);
+
+                            mTeamList.add(map);
+
+                            mHandler.sendEmptyMessage(0);
+
+                        }
+                        srl.setRefreshing(false);
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                    }
+                });
+
+
                 mRequestQueue.add(request);
+                mRequestQueue.add(request1);
                 mRequestQueue.start();
             }
         };
@@ -111,6 +197,57 @@ public class Fm03 extends Fragment implements SwipeRefreshLayout.OnRefreshListen
         public void handleMessage(Message msg) {
             Fm03_Adapter adapter = new Fm03_Adapter(context,mDataList);
             lv.setAdapter(adapter);
+
+            team01logo = (ImageView) mView.findViewById(R.id.player01logo) ;
+            team02logo = (ImageView) mView.findViewById(R.id.player02logo) ;
+            team03logo = (ImageView) mView.findViewById(R.id.player03logo) ;
+            team04logo = (ImageView) mView.findViewById(R.id.player04logo) ;
+            team05logo = (ImageView) mView.findViewById(R.id.player05logo) ;
+            team06logo = (ImageView) mView.findViewById(R.id.player06logo) ;
+
+
+
+            String url01 = mTeamList.get(0).get("player1logo").toString();
+            Glide.with(context).load(url01).into(team01logo);
+            String url02 = mTeamList.get(0).get("player2logo").toString();
+            Glide.with(context).load(url02).into(team02logo);
+            String url03 = mTeamList.get(1).get("player1logo").toString();
+            Glide.with(context).load(url03).into(team03logo);
+            String url04 = mTeamList.get(1).get("player2logo").toString();
+            Glide.with(context).load(url04).into(team04logo);
+            String url05 = mTeamList.get(2).get("player1logo").toString();
+            Glide.with(context).load(url05).into(team05logo);
+            String url06 = mTeamList.get(2).get("player2logo").toString();
+            Glide.with(context).load(url06).into(team06logo);
+
+//
+//
+//
+//
+//
+
+
+
+
+
         }
     }
+
+//    public boolean isListViewReachBottomEdge(final ListView listView) {
+//        boolean result = false;
+//        if (listView.getLastVisiblePosition() == (listView.getCount() - 2)) {
+//            final View bottomChildView = listView.getChildAt(listView.getLastVisiblePosition() - listView.getFirstVisiblePosition());
+//            result = (listView.getHeight() >= bottomChildView.getBottom());
+//        }
+//        return result;
+//    }
+//
+//    public boolean isListViewReachTopEdge(final ListView listView) {
+//        boolean result=false;
+//        if(listView.getFirstVisiblePosition()==0){
+//            final View topChildView = listView.getChildAt(0);
+//            result=topChildView.getTop()==0;
+//        }
+//        return result ;
+//    }
 }
---
+++ app/src/main/java/com/zbd/lifemanager/Json/NBAJson/TeamJson.java
@@ -0,0 +1,327 @@
+package com.zbd.lifemanager.Json.NBAJson;
+
+import java.util.List;
+
+/**
+ * Created by Amarok on 2016/12/26.
+ */
+
+public class TeamJson {
+    /**
+     * reason : 查询成功
+     * result : {"title":"NBA2015-2016赛季_圣安东尼奥马刺_赛程赛果_球队阵容","list":[{"time":"12/29 09:30","m_time":"12-29 周四 09:30","player1":"太阳","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/21.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=21","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468858","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468858","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"12/31 09:30","m_time":"12-31 周六 09:30","player1":"开拓者","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/22.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=22","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468875","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468875","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/02 07:00","m_time":"01-02 周一 07:00","player1":"马刺","player2":"老鹰","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/new/1.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=1","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468884","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468884","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/04 09:30","m_time":"01-04 周三 09:30","player1":"猛龙","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/new/28.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=28","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468901","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468901","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/06 11:30","m_time":"01-06 周五 11:30","player1":"马刺","player2":"掘金","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/7.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=7","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468919","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468919","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/08 09:30","m_time":"01-08 周日 09:30","player1":"黄蜂","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/30.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=5312","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1469635","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1469635","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"}],"more1":{"link":"http://sports.qq.com/kbsweb/?ptag=360.onebox.team.nba#nba","text":""},"more2":{"link":"http://live.3g.qq.com/g/s?aid=nba","text":""}}
+     * error_code : 0
+     */
+
+    private String reason;
+    private ResultBean result;
+    private int error_code;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+    public static class ResultBean {
+        /**
+         * title : NBA2015-2016赛季_圣安东尼奥马刺_赛程赛果_球队阵容
+         * list : [{"time":"12/29 09:30","m_time":"12-29 周四 09:30","player1":"太阳","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/21.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=21","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468858","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468858","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"12/31 09:30","m_time":"12-31 周六 09:30","player1":"开拓者","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/22.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=22","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468875","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468875","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/02 07:00","m_time":"01-02 周一 07:00","player1":"马刺","player2":"老鹰","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/new/1.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=1","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468884","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468884","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/04 09:30","m_time":"01-04 周三 09:30","player1":"猛龙","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/new/28.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=28","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468901","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468901","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/06 11:30","m_time":"01-06 周五 11:30","player1":"马刺","player2":"掘金","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/7.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=7","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1468919","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468919","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"},{"time":"01/08 09:30","m_time":"01-08 周日 09:30","player1":"黄蜂","player2":"马刺","player1logo":"http://mat1.gtimg.com/sports/nba/logo/78/30.png","player2logo":"http://mat1.gtimg.com/sports/nba/logo/78/24.png","player1url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=5312","m_player1url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","player2url":"http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24","m_player2url":"http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000","score":"VS","link1text":"视频直播","link1url":"http://sports.qq.com/kbsweb/game.htm?mid=100000:1469635","m_link1url":"http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1469635","link2text":"技术统计","link2url":"","m_link2url":"","status":"0"}]
+         * more1 : {"link":"http://sports.qq.com/kbsweb/?ptag=360.onebox.team.nba#nba","text":""}
+         * more2 : {"link":"http://live.3g.qq.com/g/s?aid=nba","text":""}
+         */
+
+        private String title;
+        private More1Bean more1;
+        private More2Bean more2;
+        private List<ListBean> list;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public More1Bean getMore1() {
+            return more1;
+        }
+
+        public void setMore1(More1Bean more1) {
+            this.more1 = more1;
+        }
+
+        public More2Bean getMore2() {
+            return more2;
+        }
+
+        public void setMore2(More2Bean more2) {
+            this.more2 = more2;
+        }
+
+        public List<ListBean> getList() {
+            return list;
+        }
+
+        public void setList(List<ListBean> list) {
+            this.list = list;
+        }
+
+        public static class More1Bean {
+            /**
+             * link : http://sports.qq.com/kbsweb/?ptag=360.onebox.team.nba#nba
+             * text :
+             */
+
+            private String link;
+            private String text;
+
+            public String getLink() {
+                return link;
+            }
+
+            public void setLink(String link) {
+                this.link = link;
+            }
+
+            public String getText() {
+                return text;
+            }
+
+            public void setText(String text) {
+                this.text = text;
+            }
+        }
+
+        public static class More2Bean {
+            /**
+             * link : http://live.3g.qq.com/g/s?aid=nba
+             * text :
+             */
+
+            private String link;
+            private String text;
+
+            public String getLink() {
+                return link;
+            }
+
+            public void setLink(String link) {
+                this.link = link;
+            }
+
+            public String getText() {
+                return text;
+            }
+
+            public void setText(String text) {
+                this.text = text;
+            }
+        }
+
+        public static class ListBean {
+            /**
+             * time : 12/29 09:30
+             * m_time : 12-29 周四 09:30
+             * player1 : 太阳
+             * player2 : 马刺
+             * player1logo : http://mat1.gtimg.com/sports/nba/logo/78/21.png
+             * player2logo : http://mat1.gtimg.com/sports/nba/logo/78/24.png
+             * player1url : http://kbs.sports.qq.com/kbsweb/teams.htm?tid=21
+             * m_player1url : http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000
+             * player2url : http://kbs.sports.qq.com/kbsweb/teams.htm?tid=24
+             * m_player2url : http://sports.qq.com/kbsweb/kbsshare/team.htm?cid=100000
+             * score : VS
+             * link1text : 视频直播
+             * link1url : http://sports.qq.com/kbsweb/game.htm?mid=100000:1468858
+             * m_link1url : http://sports.qq.com/kbsweb/kbsshare/game.htm?mid=100000:1468858
+             * link2text : 技术统计
+             * link2url :
+             * m_link2url :
+             * status : 0
+             */
+
+            private String time;
+            private String m_time;
+            private String player1;
+            private String player2;
+            private String player1logo;
+            private String player2logo;
+            private String player1url;
+            private String m_player1url;
+            private String player2url;
+            private String m_player2url;
+            private String score;
+            private String link1text;
+            private String link1url;
+            private String m_link1url;
+            private String link2text;
+            private String link2url;
+            private String m_link2url;
+            private String status;
+
+            public String getTime() {
+                return time;
+            }
+
+            public void setTime(String time) {
+                this.time = time;
+            }
+
+            public String getM_time() {
+                return m_time;
+            }
+
+            public void setM_time(String m_time) {
+                this.m_time = m_time;
+            }
+
+            public String getPlayer1() {
+                return player1;
+            }
+
+            public void setPlayer1(String player1) {
+                this.player1 = player1;
+            }
+
+            public String getPlayer2() {
+                return player2;
+            }
+
+            public void setPlayer2(String player2) {
+                this.player2 = player2;
+            }
+
+            public String getPlayer1logo() {
+                return player1logo;
+            }
+
+            public void setPlayer1logo(String player1logo) {
+                this.player1logo = player1logo;
+            }
+
+            public String getPlayer2logo() {
+                return player2logo;
+            }
+
+            public void setPlayer2logo(String player2logo) {
+                this.player2logo = player2logo;
+            }
+
+            public String getPlayer1url() {
+                return player1url;
+            }
+
+            public void setPlayer1url(String player1url) {
+                this.player1url = player1url;
+            }
+
+            public String getM_player1url() {
+                return m_player1url;
+            }
+
+            public void setM_player1url(String m_player1url) {
+                this.m_player1url = m_player1url;
+            }
+
+            public String getPlayer2url() {
+                return player2url;
+            }
+
+            public void setPlayer2url(String player2url) {
+                this.player2url = player2url;
+            }
+
+            public String getM_player2url() {
+                return m_player2url;
+            }
+
+            public void setM_player2url(String m_player2url) {
+                this.m_player2url = m_player2url;
+            }
+
+            public String getScore() {
+                return score;
+            }
+
+            public void setScore(String score) {
+                this.score = score;
+            }
+
+            public String getLink1text() {
+                return link1text;
+            }
+
+            public void setLink1text(String link1text) {
+                this.link1text = link1text;
+            }
+
+            public String getLink1url() {
+                return link1url;
+            }
+
+            public void setLink1url(String link1url) {
+                this.link1url = link1url;
+            }
+
+            public String getM_link1url() {
+                return m_link1url;
+            }
+
+            public void setM_link1url(String m_link1url) {
+                this.m_link1url = m_link1url;
+            }
+
+            public String getLink2text() {
+                return link2text;
+            }
+
+            public void setLink2text(String link2text) {
+                this.link2text = link2text;
+            }
+
+            public String getLink2url() {
+                return link2url;
+            }
+
+            public void setLink2url(String link2url) {
+                this.link2url = link2url;
+            }
+
+            public String getM_link2url() {
+                return m_link2url;
+            }
+
+            public void setM_link2url(String m_link2url) {
+                this.m_link2url = m_link2url;
+            }
+
+            public String getStatus() {
+                return status;
+            }
+
+            public void setStatus(String status) {
+                this.status = status;
+            }
+        }
+    }
+}
